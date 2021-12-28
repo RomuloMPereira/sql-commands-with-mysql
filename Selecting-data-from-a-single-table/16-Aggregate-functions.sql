5716a181be076c174f046f7a967f41ee3164789b
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-12-28 16:19:12 -0300

    Aggregate Function - AVG

--- Selecting-data-from-a-single-table/16-Aggregate-functions.sql
+++ Selecting-data-from-a-single-table/16-Aggregate-functions.sql
@@ -24,3 +24,24 @@ FROM
     FROM
         salaries
     GROUP BY salary_year) AS sub_select;
+
+SELECT 
+    DATE_FORMAT(from_date, '%Y') AS salary_year,
+    AVG(salary) AS avg_salary,
+    COUNT(*) AS total_recs,
+    SUM(salary) AS salary_sum
+FROM
+    salaries
+GROUP BY salary_year;
+
+SELECT 
+    avg_salary, salary_sum / total_recs AS calculated_avg
+FROM
+    (SELECT 
+        DATE_FORMAT(from_date, '%Y') AS salary_year,
+            AVG(salary) AS avg_salary,
+            COUNT(*) AS total_recs,
+            SUM(salary) AS salary_sum
+    FROM
+        salaries
+    GROUP BY salary_year) AS sub_select;
