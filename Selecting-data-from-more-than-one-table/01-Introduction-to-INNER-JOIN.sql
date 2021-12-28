7a9846c147eca63ac8c2c97c816faf06a8fe56d6
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-12-28 17:30:38 -0300

    INNER JOIN - Limiting Columns Displayed

--- Selecting-data-from-more-than-one-table/01-Introduction-to-INNER-JOIN.sql
+++ Selecting-data-from-more-than-one-table/01-Introduction-to-INNER-JOIN.sql
@@ -6,8 +6,19 @@ SELECT * FROM employees JOIN dept_manager ON employees.emp_no = dept_manager.emp
 SELECT 
     *
 FROM
-    employees emp
+    employees AS emp
         JOIN
-    dept_manager dm ON emp.emp_no = dm.emp_no
+    dept_manager AS dm ON emp.emp_no = dm.emp_no
 		JOIN
-	departments dept ON dm.dept_no = dept.dept_no;
+	departments AS dept ON dm.dept_no = dept.dept_no;
+    
+SELECT 
+    dept_name, emp.emp_no, first_name, last_name
+FROM
+    employees AS emp
+        JOIN
+    dept_manager AS dm ON emp.emp_no = dm.emp_no
+        JOIN
+    departments AS dept ON dm.dept_no = dept.dept_no
+ORDER BY dept_name , last_name;
+    
