21a6fd80b5f77acdabccc849c75e03bd58a59905
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-12-28 14:31:10 -0300

    DISTINCT Keyword

---
+++ Selecting-data-from-a-single-table/12-DISTINCT Keyword.sql
@@ -0,0 +1,9 @@
+use employees;
+
+SELECT DISTINCT first_name FROM employees;
+
+SELECT count(DISTINCT first_name) FROM employees;
+
+SELECT DISTINCT birth_date FROM employees;
+
+SELECT count(DISTINCT birth_date) FROM employees;
