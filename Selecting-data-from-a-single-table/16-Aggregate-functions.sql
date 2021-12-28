e5236efcc2c7d494dc0ec03611e3f5cb6dc6d557
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-12-28 16:10:05 -0300

    Aggregate Functions - MAX and MIN

--- Selecting-data-from-a-single-table/16-Aggregate-functions.sql
+++ Selecting-data-from-a-single-table/16-Aggregate-functions.sql
@@ -3,3 +3,24 @@ use employees;
 SELECT date_format(from_date, '%Y') AS from_year FROM salaries;
 
 SELECT date_format(from_date, '%Y') AS from_year, sum(salary) FROM salaries GROUP BY from_year;
+
+SELECT date_format(from_date, '%Y') AS salary_year, min(salary) FROM salaries GROUP BY salary_year;
+
+SELECT 
+    DATE_FORMAT(from_date, '%Y') AS salary_year,
+    MAX(salary) AS max_salary,
+    MIN(salary) AS min_salary
+FROM
+    salaries
+GROUP BY salary_year;
+
+SELECT 
+    salary_year, max_salary, min_salary, max_salary - min_salary AS delta
+FROM
+    (SELECT 
+        DATE_FORMAT(from_date, '%Y') AS salary_year,
+            MAX(salary) AS max_salary,
+            MIN(salary) AS min_salary
+    FROM
+        salaries
+    GROUP BY salary_year) AS sub_select;
