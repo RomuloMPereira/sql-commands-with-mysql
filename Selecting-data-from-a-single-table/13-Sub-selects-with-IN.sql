9c0f93c63a401969aa8151e447c555fbf18f2dc8
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-12-28 14:45:51 -0300

    WHERE Clause - Sub-selects with IN

---
+++ Selecting-data-from-a-single-table/13-Sub-selects-with-IN.sql
@@ -0,0 +1,5 @@
+use employees;
+
+SELECT DISTINCT first_name FROM employees WHERE first_name LIKE 'E%';
+
+SELECT * FROM employees WHERE first_name IN (SELECT DISTINCT first_name FROM employees WHERE first_name LIKE 'E%');
