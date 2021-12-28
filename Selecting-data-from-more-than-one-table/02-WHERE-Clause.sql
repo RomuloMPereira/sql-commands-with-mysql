78b05767c37ee577370b2f9c0b1a70c4734bd099
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-12-28 17:56:39 -0300

    Assignment - Create Department Employee Report

--- Selecting-data-from-more-than-one-table/02-WHERE-Clause.sql
+++ Selecting-data-from-more-than-one-table/02-WHERE-Clause.sql
@@ -28,3 +28,29 @@ WHERE
     dm.to_date = '9999-01-01'
         AND emp.gender = 'F'
 ORDER BY dept_name;
+
+SELECT * FROM dept_emp JOIN departments ON dept_emp.dept_no = departments.dept_no;
+
+SELECT 
+    *
+FROM
+    dept_emp
+        JOIN
+    departments ON dept_emp.dept_no = departments.dept_no
+		JOIN 
+	employees ON dept_emp.emp_no = employees.emp_no;
+
+SELECT 
+    dept_name, first_name, last_name, title
+FROM
+    dept_emp
+        JOIN
+    departments ON dept_emp.dept_no = departments.dept_no
+        JOIN
+    employees ON dept_emp.emp_no = employees.emp_no
+        JOIN
+    titles ON employees.emp_no = titles.emp_no
+WHERE
+    dept_emp.to_date = '9999-01-01'
+        AND titles.to_date = '9999-01-01'
+ORDER BY dept_name , last_name;
