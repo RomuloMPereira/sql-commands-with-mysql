4a6a39ea14e86eca7f57d1c2d4079e929c3e1b94
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-12-28 17:23:51 -0300

    INNER JOIN - Using Table Aliases

--- Selecting-data-from-more-than-one-table/01-Introduction-to-INNER-JOIN.sql
+++ Selecting-data-from-more-than-one-table/01-Introduction-to-INNER-JOIN.sql
@@ -2,3 +2,12 @@ use employees;
 
 SELECT * FROM employees INNER JOIN dept_manager ON employees.emp_no = dept_manager.emp_no;
 SELECT * FROM employees JOIN dept_manager ON employees.emp_no = dept_manager.emp_no;
+
+SELECT 
+    *
+FROM
+    employees emp
+        JOIN
+    dept_manager dm ON emp.emp_no = dm.emp_no
+		JOIN
+	departments dept ON dm.dept_no = dept.dept_no;
