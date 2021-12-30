8a9d0cac0bbc0b9f105591c08a7fc6d6e4380b85
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-12-30 14:42:33 -0300

    EQUI JOIN

--- Selecting-data-from-more-than-one-table/04-EQUI-JOIN.sql
+++ Selecting-data-from-more-than-one-table/04-EQUI-JOIN.sql
@@ -14,3 +14,18 @@ WHERE
         AND de.to_date = '9999-01-01'
         AND t.to_date = '9999-01-01'
 ORDER BY dept_name , last_name;
+
+SELECT 
+    count(*)
+FROM
+    dept_emp AS de,
+    departments AS dept,
+    employees AS emp,
+    titles AS t
+WHERE
+    emp.emp_no = de.emp_no
+        AND de.dept_no = dept.dept_no
+        AND t.emp_no = emp.emp_no
+        AND de.to_date = '9999-01-01'
+        AND t.to_date = '9999-01-01'
+ORDER BY dept_name , last_name;
