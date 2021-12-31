aa5f01148574b7bc00c1efe0bd52357f3acbd9f9
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-12-31 14:35:55 -0300

    INSERT using Sub-select (incrementing the primary key)

--- Inserting-updating-and-deleting-data/01-INSERT.sql
+++ Inserting-updating-and-deleting-data/01-INSERT.sql
@@ -3,3 +3,4 @@ use employees;
 INSERT INTO departments VALUES ('d999', 'Awesome Gurus');
 INSERT INTO departments (dept_no, dept_name)  VALUES ('d998', 'Awesome Gurus 2');
 
+INSERT INTO employees SELECT max(emp_no) + 1, '1988-01-01', 'Bruno', 'Silva', 'M', '2021-12-31' FROM employees;
