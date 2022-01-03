d41176cab78bb1cbcb9e8b34328e45348de74166
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-01-03 16:36:14 -0300

    Assignment - Insert new salary record and update the previous record

--- Inserting-updating-and-deleting-data/02-UPDATE.sql
+++ Inserting-updating-and-deleting-data/02-UPDATE.sql
@@ -11,3 +11,12 @@ SELECT * FROM employees WHERE first_name = 'Jessie' and last_name = 'Porter';
 UPDATE employees SET first_name = 'Sam', last_name = 'Axe' WHERE emp_no = 500001;
 
 SELECT * FROM employees WHERE emp_no = 500001;
+
+-- Assignment - Insert new salary record and update to_date of previous record
+
+SELECT * FROM employees WHERE first_name = 'Sam';
+SELECT * FROM salaries WHERE emp_no = 500001;
+
+INSERT INTO salaries VALUES (500001, 140000, '2019-01-01', '9999-01-01');
+
+UPDATE salaries SET to_date = '2019-01-01', from_date = '2018-01-01' WHERE emp_no = 500001;
