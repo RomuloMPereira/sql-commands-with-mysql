efa4cbbfc4faee9470af64b7beda0030b4f8c731
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-01-03 15:17:59 -0300

    Assignment - UPDATE Statement to change employee name

--- Inserting-updating-and-deleting-data/02-UPDATE.sql
+++ Inserting-updating-and-deleting-data/02-UPDATE.sql
@@ -3,3 +3,11 @@ use employees;
 SELECT * FROM departments WHERE dept_no = 'd999';
 
 UPDATE departments SET dept_name = 'Social Media Marketing' WHERE dept_no = 'd999';
+
+
+-- Assignment - Update Statement to change employee name
+SELECT * FROM employees WHERE first_name = 'Jessie' and last_name = 'Porter';
+
+UPDATE employees SET first_name = 'Sam', last_name = 'Axe' WHERE emp_no = 500001;
+
+SELECT * FROM employees WHERE emp_no = 500001;
